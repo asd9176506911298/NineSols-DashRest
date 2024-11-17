b05e89966577e9f68671ded9f4bf2f68ca1dd473
Yuki.kaco <a0936660023@gmail.com>
2024-11-17 21:03:28 +0800

    Init

---
+++ Source/DashRest.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using NineSolsAPI;
+using UnityEngine;
+
+namespace DashRest;
+
+[BepInDependency(NineSolsAPICore.PluginGUID)]
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class DashRest : BaseUnityPlugin {
+    private ConfigEntry<bool> enableSomethingConfig = null!;
+    private ConfigEntry<KeyboardShortcut> somethingKeyboardShortcut = null!;
+
+    private Harmony harmony = null!;
+
+    private void Awake() {
+        Log.Init(Logger);
+        RCGLifeCycle.DontDestroyForever(gameObject);
+
+        // Load patches from any class annotated with @HarmonyPatch
+        harmony = Harmony.CreateAndPatchAll(typeof(DashRest).Assembly);
+
+        enableSomethingConfig = Config.Bind("General.Something", "Enable", true, "Enable the thing");
+        somethingKeyboardShortcut = Config.Bind("General.Something", "Shortcut",
+            new KeyboardShortcut(KeyCode.H, KeyCode.LeftControl), "Shortcut to execute");
+
+        KeybindManager.Add(this, TestMethod, () => somethingKeyboardShortcut.Value);
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+
+    private void TestMethod() {
+        if (!enableSomethingConfig.Value) return;
+        ToastManager.Toast("Shortcut activated");
+
+    }
+
+    private void OnDestroy() {
+
+        harmony.UnpatchSelf();
+    }
+}
